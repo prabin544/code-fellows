ce4167e2635dba2c0b709ee23bb035ceed830682
Prabin Singh <pravin544@gmail.com>
2021-02-08 16:14:40 -0600

    make it better

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ require('./services/passport')
 
 require('./routes/authRoute')(app)
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
+
 app.listen(PORT,()=>{
     console.log("server running on "+ PORT)
 })
