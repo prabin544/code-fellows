d2659868f592de952dab2d97c5182bfae195b144
Prabin Singh <pravin544@gmail.com>
2021-02-10 00:42:16 -0600

    changed card layout

--- client/src/Page/Cardpage.js
+++ client/src/Page/Cardpage.js
@@ -1,36 +1,23 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Card, CardDeck } from 'react-bootstrap';
 import './CardPage.css';
 import { FaSave } from "react-icons/fa";
 
 function CardPage (props) {
-
+    console.log(props)
     return(
-        <div class="container-fluid">
-                <div class="row">
-                <div class="col-12 mt-4">
-                    <div class="card" >
-                        <div  class="card-header">
-                            <h5 type="title" >{props.title}</h5>
-                        </div>
-                        <div class="card-horizontal">
-                            <div class="img-square-wrapper">
-                                <img type="image"  class="" src={props.image} responsive style={{ width: 200}} resizeMode="contain" alt="Card image cap"/>
-                            </div>
-                            <div class="card-body">
-                                < FaSave style={{
-                                    position: 'absolute',
-                                    right: 5,
-                                    top: 5,
-                                }} />
-                                <h6 type="author"  class="card-title">{props.author}</h6>
-                                <p type="abstract"  class="card-text">{props.abstract}</p>
-                            </div>
-                        </div>
-                        <Button type="url" variant="info" href={props.url}>View</Button>
-                    </div>
-                </div>
-            </div>
+        <div className="container">
+            <Card  className="col-sm-4 py-2">
+                <Card.Header><h6>{props.title}</h6></Card.Header>
+                <Card.Img variant="top" src={props.image} />
+                <Card.Body>
+                    <Card.Title>{props.author}</Card.Title>
+                    <Card.Text>
+                    {props.abstract}
+                    </Card.Text>
+                    <Button type="url" variant="info" href={props.url}>View</Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 
